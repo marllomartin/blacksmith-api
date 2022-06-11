55f5e0a03a50e1a6a538d9faae5fac440340feab
marllomartin <marllon3636@gmail.com>
2022-06-11 17:05:54 -0300

    feat(services/product.service): Inicia service de Produtos - getAll

---
+++ src/services/product.service.ts
@@ -0,0 +1,19 @@
+import connection from '../models/connection';
+import ProductModel from '../models/product.model';
+import Product from '../interfaces/product.interface';
+
+class ProductService {
+  public model: ProductModel;
+
+  constructor() {
+    this.model = new ProductModel(connection);
+  }
+
+  public async getAll(): Promise<Product[]> {
+    const products = await this.model.getAll();
+
+    return products;
+  }
+}
+
+export default ProductService;
