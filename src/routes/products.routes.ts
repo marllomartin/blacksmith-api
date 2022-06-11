c7db1aeca8b35a2b563f5f684232e0cbff7c95fa
marllomartin <marllon3636@gmail.com>
2022-06-11 17:08:02 -0300

    feat(routes/product.routes): Inicia rotas de Produtos - getAll

---
+++ src/routes/products.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import ProductController from '../controllers/product.controller';
+
+const router = Router();
+
+const productController = new ProductController();
+
+// Get All Products
+router.get('/products', productController.getAll)
+
+export default router;
