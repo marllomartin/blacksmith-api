a78810615d028d27711bf245b9e4a436164ad265
marllomartin <marllon3636@gmail.com>
2022-06-11 17:07:12 -0300

    feat(controllers/product.controller): Inicia controller de Produtos - getAll

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import ProductService from '../services/product.service';
+
+class ProductController {
+  constructor(private productService = new ProductService()) { }
+
+  public getAll = async (req: Request, res: Response) => {
+    const products = await  this.productService.getAll();
+
+    res.status(StatusCodes.OK).json(products);
+  }
+}
+
+export default ProductController;
