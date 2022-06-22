a5291121c23a106698fb480341a87fa2fbc5809b
marllomartin <marllon3636@gmail.com>
2022-06-22 03:46:57 -0300

    feat(controllers/product.controller):  Controller de criar Produto - create

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -4,10 +4,17 @@ import ProductService from '../services/product.service';
 class ProductController {
   constructor(private productService = new ProductService()) { }
 
-  public getAll = async (req: Request, res: Response) => {
+  public getAll = async (_req: Request, res: Response) => {
     const products = await this.productService.getAll();
 
     res.status(200).json(products);
+  };
+
+  public create = async (req: Request, res: Response) => {
+    const { name, amount } = req.body;
+    const newProduct = await this.productService.create({ name, amount });
+
+    res.status(201).json(newProduct);
   };
 }
 
