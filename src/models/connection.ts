cd56041dcf08eefada8d89f29ee43ec4619335ca
marllomartin <marllon3636@gmail.com>
2022-06-21 21:25:41 -0300

    refactor(models/connection.ts): Fix database connection parameters

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,12 +1,12 @@
-import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
 
 dotenv.config();
 
 const connection = mysql.createPool({
-  host: process.env.DB_HOSTNAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
 });
 
 export default connection;
