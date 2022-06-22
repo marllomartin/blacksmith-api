eb4088c09c9b8a547539f110c6d5719565fedc0a
marllomartin <marllon3636@gmail.com>
2022-06-22 20:59:48 -0300

    feat(src/app): Atribui rotas de Usuários ao App

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import ProductRoutes from './routes/products.routes';
+import UserRoutes from './routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use(ProductRoutes);
+app.use(ProductRoutes, UserRoutes);
 
 export default app;
