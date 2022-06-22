09853b7b6bd3829660ff0341531237a84e76a7f9
marllomartin <marllon3636@gmail.com>
2022-06-22 20:59:10 -0300

    feat(routes/users.routes): Cria rota de Usuários - create

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import UserController from '../controllers/user.controller';
+import validateUser from '../middlewares/users.middleware';
+
+const router = Router();
+
+const { validateUsername, validateClasse, validateLevel, validatePassword } = validateUser;
+
+const userController = new UserController();
+
+// Create User
+router.post(
+  '/users',
+  validateUsername,
+  validateClasse,
+  validateLevel,
+  validatePassword,
+
+  userController.create,
+);
+
+export default router;
