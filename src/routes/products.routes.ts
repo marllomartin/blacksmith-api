47cf3529563455c15319915d091f178147db8897
marllomartin <marllon3636@gmail.com>
2022-06-22 03:49:28 -0300

    feat(routes/product.routes): Implementa rota de criar novo Produto

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import validateProduct from '../middlewares/products.middleware';
 
 const router = Router();
+
+const { validateName, validateAmount } = validateProduct;
 
 const productController = new ProductController();
 
 // Get All Products
 router.get('/products', productController.getAll);
+
+// Create Product
+router.post('/products', validateName, validateAmount, productController.create);
 
 export default router;
