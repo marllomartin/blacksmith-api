8ca51ee8fdb55584e2f5d30827cb16c5cbc52ec5
marllomartin <marllon3636@gmail.com>
2022-06-24 14:46:19 -0300

    feat(models/user.model): Cria função de buscar Usuário - findUser

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,5 @@
 import { Pool, ResultSetHeader } from 'mysql2/promise';
+import Login from '../interfaces/login.interface';
 import User from '../interfaces/user.interface';
 
 class UserModel {
@@ -15,6 +16,16 @@ class UserModel {
       'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?)',
       [username, classe, level, password],
     );
+  }
+
+  public async findUser(login: Login): Promise<Login[]> {
+    const { username, password } = login;
+
+    const result = await this.connection.execute(`SELECT * FROM Trybesmith.Users 
+    WHERE username = ? AND password = ?`, [username, password]);
+
+    const [rows] = result;
+    return rows as Login[];
   }
 }
 
