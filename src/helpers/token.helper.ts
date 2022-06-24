d194227b72165a502ea71f9bc6a0fa69fd3bd921
marllomartin <marllon3636@gmail.com>
2022-06-24 14:46:59 -0300

    feat(src/helpers): Ajusta payload da função auxiliar de JWT

--- src/helpers/token.helper.ts
+++ src/helpers/token.helper.ts
@@ -1,9 +1,8 @@
 import { sign } from 'jsonwebtoken';
-import User from '../interfaces/user.interface';
 
 const secret = 'banana';
 
-function generateToken(payload: User) {
+function generateToken(payload: object) {
   const token = sign({ data: payload }, secret, {
     expiresIn: '1h',
     algorithm: 'HS256',
