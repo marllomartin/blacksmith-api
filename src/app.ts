b0ef1091a7bb9c60269b1216967c808947d614c5
marllomartin <marllon3636@gmail.com>
2022-06-23 21:47:13 -0300

    feat(src/app): Atribui rotas de Pedidos ao App

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import ProductRoutes from './routes/products.routes';
 import UserRoutes from './routes/users.routes';
+import OrderRoutes from './routes/orders.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use(ProductRoutes, UserRoutes);
+app.use(ProductRoutes, UserRoutes, OrderRoutes);
 
 export default app;
