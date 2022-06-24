df3a99846ad994c058e2c86b41882d22e6b05b15
marllomartin <marllon3636@gmail.com>
2022-06-24 14:47:29 -0300

    feat(services/user.service): Cria Service de Login

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,6 +2,7 @@ import connection from '../models/connection';
 import UserModel from '../models/user.model';
 import User from '../interfaces/user.interface';
 import generateToken from '../helpers/token.helper';
+import Login from '../interfaces/login.interface';
 
 class UserService {
   public model: UserModel;
@@ -14,6 +15,18 @@ class UserService {
     await this.model.create(user);
 
     const token = generateToken(user);
+
+    return { token };
+  }
+
+  public async login(login: Login): Promise<object> {
+    const verify = await this.model.findUser(login);
+
+    if (verify.length === 0) {
+      throw new Error();
+    }
+
+    const token = generateToken(login);
 
     return { token };
   }
